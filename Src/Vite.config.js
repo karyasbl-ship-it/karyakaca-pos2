5ae0be0703deb7a2fa543c45a8261cef7628f36b
karyasbl-ship-it <karya.sbl@gmail.com>
2025-09-02 15:27:51 +0700

    Update Vite.config.js

--- Src/Vite.config.js
+++ Src/Vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/karyakaca-pos2/', // <- harus sama persis dengan nama repo
+  base: '/karyakaca-pos2/', // Ganti sesuai nama repo kamu
 })
